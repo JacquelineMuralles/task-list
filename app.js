aafd17d41f3ce83f840227f1c03827a1a9fb4e09
JacquelineMuralles <jacquelinenmuralles@gmail.com>
2022-03-15 17:08:41 -0500

    added functionality to delete, clear, and filter tasks

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ loadEventListeners();
 function loadEventListeners() {
   // Add task event
   form.addEventListener('submit', addTask);
+  //Remove task event
+  taskList.addEventListener('click', removeTask);
+  //Clear task event
+  clearBtn.addEventListener('click', clearTasks);
+  //Filter tasks event
+  filter.addEventListener('keyup', filterTasks);
 }
 
 // Add task
@@ -19,7 +25,6 @@ function addTask(e) {
   if(taskInput.value === ''){
     alert('Add a task');
   }
-
   // Create li element
   const li = document.createElement('li');
   // Add class
@@ -34,12 +39,42 @@ function addTask(e) {
   link.innerHTML = '<i class="fa-solid fa-circle-xmark"></i>';
   //Append the link to li 
   li.appendChild(link);
-
   //Append li to ul
   taskList.appendChild(li);
-  
   //Clear Input
   taskInput.value = '';
-
   e.preventDefault();
+}
+
+//Remove Task
+function removeTask(e) {
+  if(e.target.parentElement.classList.contains('delete-item')) {
+    if(confirm('Are you sure you want to delete?')) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+//Clear tasks
+function clearTasks() {
+  // tasklist.innerHTML = '';
+
+  //Faster for chrome
+  while(taskList.firstChild){
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+//Filter tasks
+function filterTasks(e){
+  const text = e.target.value.toLowerCase();
+  document.querySelectorAll('.collection-item').forEach(function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1){
+      task.style.display = 'block';
+    } else{
+      task.style.display = 'none';
+    }
+  });
+
 }
